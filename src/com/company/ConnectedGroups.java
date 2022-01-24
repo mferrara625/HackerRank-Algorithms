ebcc8047ea07dbaf4c0a93be0e4d20cba3db5ecd
mferrara625 <mferrara401@yahoo.com>
2022-01-23 21:11:00 -0500

    worked on connected groups

--- src/com/company/ConnectedGroups.java
+++ src/com/company/ConnectedGroups.java
@@ -2,15 +2,26 @@ package com.company;
 
 public class ConnectedGroups {
 
-    public int connectedGroups(int n, String[] related){
-        char[] groupOne = related[0].toCharArray();
-        for(int j = 1; j < n; j++){
-            char[] letters = related[j].toCharArray();
-            for(int i = 0; i < n; i++){
-                if(letters[i] == '1' && i != j){
 
+    public int connectedGroups(int n, String[] related) {
+        int groupCount = 1;
+        boolean isConnected;
+        char[] groupOne = related[0].toCharArray();
+        for (int j = 1; j < n; j++) {
+            isConnected = false;
+            char[] letters = related[j].toCharArray();
+            for (int i = 0; i < n; i++) {
+                if (letters[i] == '1' && groupOne[i] == '1') {
+                    isConnected = true;
+                    for (int k = 0; k < n; n++)
+                        if (letters[k] == '1')
+                            groupOne[k] = '1';
                 }
+
             }
+            if (!isConnected)
+                groupCount++;
         }
+        return groupCount;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -42,18 +42,28 @@ public class Main {
 //        int n = 4;
 //        String[] related = {"1100", "1110", "0110", "0001"};
 
+        ConnectedGroups cg = new ConnectedGroups();
+//        System.out.println(cg.connectedGroups(5, new String[]{"10000", "01000", "00100", "00010", "00001"}));
+//        System.out.println(cg.connectedGroups(4, new String[]{"1100", "1110", "0110", "0001"}));
+
+        System.out.println(cg.connectedGroups(4, new String[]{"1100", "1100", "0010", "0001"}));
+
         //         CHALLENGE 7
-        WagonR wagonR = new WagonR(70);
-        wagonR.getMileage();
-        HondaCity hondaCity = new HondaCity(89);
-        hondaCity.getMileage();
-        InnovaCrysta innovaCrysta = new InnovaCrysta(95);
-        innovaCrysta.getMileage();
+//        WagonR wagonR = new WagonR(70);
+//        wagonR.getMileage();
+//        HondaCity hondaCity = new HondaCity(89);
+//        hondaCity.getMileage();
+//        InnovaCrysta innovaCrysta = new InnovaCrysta(95);
+//        innovaCrysta.getMileage();
 
         //         CHALLENGE 8
 //        Customer Credit Limit
 //
 //        SELECT id, name FROM customers WHERE country = 'USA' AND credits >= 100000 ORDER BY id;
+
+        //         CHALLENGE 9
+
+
 
 
     }
